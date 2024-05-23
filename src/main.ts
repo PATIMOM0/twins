aa15f0572728a714aec6ff54a7ae7697a60fe489
PATIMOM0 <gaeloso290@gmail.com>
2024-05-23 08:27:16 -0600

    me arte

--- src/main.ts
+++ src/main.ts
@@ -4,3 +4,15 @@ import { AppComponent } from './app/app.component';
 
 bootstrapApplication(AppComponent, appConfig)
   .catch((err) => console.error(err));
+
+  import {Component} from '@angular/core';
+
+/**
+ * @title Multi-row toolbar
+ */
+@Component({
+  selector: 'navbar',
+  templateUrl: 'navbar.html',
+  styleUrls: ['styles.css'],
+})
+export class ToolbarMultirowExample {}
